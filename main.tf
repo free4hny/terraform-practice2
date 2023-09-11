656af8718d8ae4cda407dec2d0f5f792f8a71730
Abhishek Bhardwaj <37928990+free4hny@users.noreply.github.com>
2023-09-11 16:57:15 -0400

    vsvsdf

--- main.tf
+++ main.tf
@@ -1,24 +1,16 @@
-resource "aws_rds_cluster" "ts_cluster" {
-  cluster_identifier              = "my-aurora-cluster"
-  engine                          = "aurora-postgresql"
-  master_username                 = "postgres"
-  master_password                 = "password"
-  skip_final_snapshot             = true
-  vpc_security_group_ids          = ["ssg-05bdf8bfbd66de4e5"]  # Your existing security group
-  db_subnet_group_name            = "tssubnetgroup" # Your existing DB subnet group name
-  availability_zones              = ["us-east-1a", "us-east-1b"]    # Adjust based on your region and available zones
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"  # This is an example Amazon Linux 2 AMI ID, replace with the desired AMI ID
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-example"
+  }
 }
 
-resource "aws_rds_cluster_instance" "aurora_instance" {
-  cluster_identifier              = aws_rds_cluster.ts_cluster.id
-  identifier                      = "my-aurora-instance-ts"
-  instance_class                  = "db.t2.small"
-  engine                          = "aurora-postgresql"
-  publicly_accessible             = false
-  db_subnet_group_name            = "tssubnetgroup"  # Your existing DB subnet group name
+output "instance_public_ip" {
+  value = aws_instance.example.public_ip
 }
 
-# Output the endpoint for connection
-output "aurora_endpoint" {
-  value = aws_rds_cluster.ts_cluster.endpoint
+output "instance_public_dns" {
+  value = aws_instance.example.public_dns
 }
