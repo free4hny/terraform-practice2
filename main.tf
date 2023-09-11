afd8a5f52db8fa9ea98be763162de39f35930e65
Abhishek Bhardwaj <37928990+free4hny@users.noreply.github.com>
2023-09-11 15:14:34 -0400

    fs

--- main.tf
+++ main.tf
@@ -1,38 +1,28 @@
-module "aurora_cluster" {
-  source  = "terraform-aws-modules/rds-aurora/aws"
+provider "aws" {
+  region = "us-east-1"  # Adjust to your preferred region
+}
 
-  name           = "test-aurora-db-postgres-TS"
-  engine         = "aurora-postgresql"
-  engine_version = "14.5"
-  username = "${var.aurora_username}"
-  password = "${var.password}"
-  instance_class = "db.t2.small"
-  instances = {
-    one = {}
-    2 = {
-      instance_class = "db.t2.small"
-    }
-  }
+resource "aws_rds_cluster" "TS_cluster" {
+  cluster_identifier              = "my-aurora-cluster"
+  engine                          = "aurora-postgresql"
+  master_username                 = "postgres"
+  master_password                 = "password"
+  skip_final_snapshot             = true
+  vpc_security_group_ids          = ["ssg-05bdf8bfbd66de4e5"]  # Your existing security group
+  db_subnet_group_name            = "tssubnetgroup" # Your existing DB subnet group name
+  availability_zones              = ["us-east-1a", "us-east-1b"]    # Adjust based on your region and available zones
+}
 
-  vpc_id               = "vpc-08a1b8d567baacd6e"
-  db_subnet_group_name = "db-subnet-group-TS"
-  security_group_rules = {
-    ex1_ingress = {
-      cidr_blocks = ["10.20.0.0/20"]
-    }
-    ex1_ingress = {
-      source_security_group_id = "sg-05bdf8bfbd66de4e5"
-    }
-  }
+resource "aws_rds_cluster_instance" "aurora_instance" {
+  cluster_identifier              = aws_rds_cluster.aurora_cluster.id
+  identifier                      = "my-aurora-instance-TS"
+  instance_class                  = "db.t2.small"
+  engine                          = "aurora-postgresql"
+  publicly_accessible             = false
+  db_subnet_group_name            = "tssubnetgroup"  # Your existing DB subnet group name
+}
 
-  storage_encrypted   = true
-  apply_immediately   = true
-  monitoring_interval = 10
-
-  enabled_cloudwatch_logs_exports = ["postgresql"]
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
+# Output the endpoint for connection
+output "aurora_endpoint" {
+  value = aws_rds_cluster.TS_cluster.endpoint
 }
