4b369bdef61df27465153dd0eca5f1236e8b4278
Abhishek Bhardwaj <37928990+free4hny@users.noreply.github.com>
2023-09-11 14:50:24 -0400

    var fixed

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "db_name" {}
 variable "db_user" {}
 variable "db_password" {}
 
-variable "environment name" {
+variable "environment_name" {
     description = "Name of the environment"
 }
 
