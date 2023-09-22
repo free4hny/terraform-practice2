843cf632d0232cfc0eb0663386b5e5177a581fb5
Abhishek Bhardwaj <37928990+free4hny@users.noreply.github.com>
2023-09-22 10:27:45 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-1"  # Change to your desired AWS region
-  access_key = "AKIAX4DSBSYWFDZH7LML"
-  secret_key = "+Bvv1V3MbyjNZpgBqQ/J3UENvvY0GnkfdvOJUQOL"
+  access_key = ""
+  secret_key = ""
 }
